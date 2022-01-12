5bc8599412f5ef2720f7a5e0d032f7f2b59da80e
Skyler Crane <skylercrane7@gmail.com>
2022-01-12 01:08:19 -0700

    Included navbar and home page component in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/common/Navbar.vue'
+import HomePage from './pages/HomePage.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <header>
+        <Navbar />
+    </header>
+    <main class="min-h-screen bg-primary">
+        <HomePage />
+    </main>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
