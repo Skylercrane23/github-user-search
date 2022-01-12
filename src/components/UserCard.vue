cd97dad94ab31ac0b539e4a61a61222bf5e0a2e4
Skyler Crane <skylercrane7@gmail.com>
2022-01-12 14:30:38 -0700

    Added userCard with basic info from api.

---
+++ src/components/UserCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <li class="bg-primary hover:bg-secondary hover:bg-opacity-10 border-2 border-secondary rounded-lg">
+        <a
+            :href="user.html_url"
+            target="_blank"
+            class="text-center py-10 px-6 xl:px-10 flex items-center justify-center"
+        >
+            <div class="space-y-6 xl:space-y-10 max-w-full">
+                <img class="mx-auto h-40 w-40 rounded-full border-2 border-secondary" :src="user.avatar_url" alt="" />
+                <p class="text-white font-bold text-2xl leading-relaxed truncate break-word">{{ user.login }}</p>
+            </div>
+        </a>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
