bfde3316a17a9fac6399865198d8f1f6f96dabfc
Skyler Crane <skylercrane7@gmail.com>
2022-01-12 14:29:09 -0700

    Added optional params with defaults.

--- src/api/userSearch.js
+++ src/api/userSearch.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /**
 * @return {array} Array of admin table types 
 */
-export function getUserByQuery(query) {
-    return axios.get('https://api.github.com/search/users', { params: { q: query } })
+export function getUserByQuery({ query, per_page = 18, page = 1 }) {
+    return axios.get('https://api.github.com/search/users', { params: { q: query, per_page, page } })
 }
 
