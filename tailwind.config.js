bae4e9ce8d731c6fdc2a985a17737461a92e7353
Skyler Crane <skylercrane7@gmail.com>
2022-01-12 01:06:55 -0700

    Added tailwind css and axios

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0a1930',
+                secondary: '#f46663'
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+}
