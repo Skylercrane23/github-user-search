93dca8d1244a5ab5126f96cd7d78099419a21e1d
Skyler Crane <skylercrane7@gmail.com>
2022-01-12 01:07:53 -0700

    Created base navbar component.

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="bg-primary">
+        <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between h-16">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0 flex items-center">
+                        <img class="block h-12 w-auto" src="/images/GitHub_Logo_White.png" alt="Github Logo" />
+                    </div>
+
+                    <div class="block ml-4">
+                        <div class="text-white tracking-wider rounded-md text-base">Take Home</div>
+                    </div>
+                </div>
+
+                <a href="https://github.com/Skylercrane23/github-user-search" target="_blank">
+                    <img class="h-10 w-10" src="/images/GitHub-Mark-Light-120px-plus.png" alt="GitHub" />
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
